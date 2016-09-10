e156b0e39c0f36be1af32486508d71adc54b9b5f
BitHashTech <m.y.yousry96@gmail.com>
2016-09-10 17:44:31 +0200

    Revert "add dev"
    This reverts commit 31679fa815f20a6ca82c9ad8f512f8362b47d094.

---
+++ test/test/Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<set>
+#include<sstream>
+#include<algorithm>
+#include<stdio.h>
+#include <cstring>
+#include<functional>
+#include<queue>
+#include<iterator>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+}
